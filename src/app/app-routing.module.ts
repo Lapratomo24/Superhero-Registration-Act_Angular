3debcbfcd5b6d065f6520a16bc40eea79f0a92c0
luthfi.pratomo@telekom.de <luthfi.pratomo@telekom.de>
2023-10-03 20:01:48 +0200

    add navbar and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSuperheroComponent } from './list-superhero/list-superhero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'superheroes', component: ListSuperheroComponent},
+  {path: '', redirectTo: 'superheroes', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
